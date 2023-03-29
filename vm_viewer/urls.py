dc2310e0a897ffff156511d25bb11d6ae462e872
Brynys <j.janata.55@gmail.com>
2023-03-29 16:55:52 +0200

    Start/Stop

--- vm_viewer/urls.py
+++ vm_viewer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_vms, name='get_vms'),
+    path('toggle_vm/<int:vmid>/<str:action>/<str:node>/', views.toggle_vm_view, name='toggle_vm'),
 ]
--- vm_viewer/views.py
+++ vm_viewer/views.py
@@ -1,19 +1,46 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
 from proxmoxer import ProxmoxAPI
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+api_url = '192.168.5.51:8006'
+api_user = 'Api-jj@pve'
+api_password = '123456'
 
 def get_vms(request):
-    api_user = 'Api-jj@pve'
-    api_password = '123456'
-    api_url = '192.168.5.51'
     proxmox = ProxmoxAPI(api_url, user=api_user, password=api_password, verify_ssl=False)
 
     vms = []
     for node in proxmox.nodes.get():
         for vm in proxmox.nodes(node['node']).qemu.get():
+            vm['node'] = node['node']
             vms.append(vm)
+    vms.sort(key=lambda x: x['vmid'])  # Přidáno
 
-    context = {'vms': vms}
+    containers = []
+    for node in proxmox.nodes.get():
+        for container in proxmox.nodes(node['node']).lxc.get():
+            container['node'] = node['node']
+            containers.append(container)
+    containers.sort(key=lambda x: x['vmid'])  # Přidáno
+
+    context = {'vms': vms, 'containers': containers}
     return render(request, 'vm_viewer/index.html', context)
+
+def toggle_vm(api, vmid, action, node):
+    if action == 'start':
+        api.nodes(node).qemu(vmid).status.start.post()
+    elif action == 'stop':
+        api.nodes(node).qemu(vmid).status.stop.post()
+
+def toggle_vm_view(request, vmid, action, node):
+    api = ProxmoxAPI(api_url, user=api_user, password=api_password, verify_ssl=False)
+    toggle_vm(api, vmid, action, node)
+    return redirect('get_vms')
+
+def get_containers(api):
+    containers = []
+    for node in api.nodes.get():
+        node_containers = api.nodes(node['node']).lxc.get()
+        containers.extend(node_containers)
+    return containers
