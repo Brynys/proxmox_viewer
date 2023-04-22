86d8195f05f59b208d75c7e151326b4463af903b
Brynys <jonasc143@gmail.com>
2023-04-23 00:46:18 +0200

    Začátek vytváření vm pomocí popup formu

--- vm_viewer/urls.py
+++ vm_viewer/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('bubles/', views.bubles, name='bubles'),
+    path('create_vms/', views.create_vms, name='create_vms'),
     path('get_vms/', views.get_vms, name='get_vms'),
     path('toggle_vm/<int:vmid>/<str:action>/<str:node>/', views.toggle_vm_view, name='toggle_vm'),
     path('table/', views.table, name='table'),
--- vm_viewer/views.py
+++ vm_viewer/views.py
@@ -3,6 +3,7 @@ from proxmoxer import ProxmoxAPI
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.core.serializers.json import DjangoJSONEncoder
+from django.views.decorators.csrf import csrf_exempt
 import json
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -15,25 +16,26 @@ api_password = '123456'
 
 def get_vms(request):
     proxmox = ProxmoxAPI(api_url, user=api_user, password=api_password, verify_ssl=False)
+    
+    nodes = sorted(proxmox.nodes.get(), key=lambda x: int(x['node'].replace('pve', '')))
+
     vms = []
-    for node in proxmox.nodes.get():
+    for node in nodes:
         for vm in proxmox.nodes(node['node']).qemu.get():
             vm['node'] = node['node']
             vms.append(vm)
-    vms.sort(key=lambda x: x['vmid'])  
+    vms.sort(key=lambda x: x['vmid'])
 
     containers = []
-    for node in proxmox.nodes.get():
+    for node in nodes:
         for container in proxmox.nodes(node['node']).lxc.get():
             container['node'] = node['node']
             containers.append(container)
-    containers.sort(key=lambda x: x['vmid'])  
-
-    nodes = proxmox.nodes.get()
+    containers.sort(key=lambda x: x['vmid'])
 
     context = {'nodes': nodes, 'vms': vms, 'containers': containers}
     return JsonResponse(context, safe=False)
-    render(request, 'vm_viewer/table.html', context)
+    
         
 
 def toggle_vm(api, vmid, action, node):
@@ -53,12 +55,32 @@ def toggle_vm_view(request, vmid, action, node):
     else:
         return JsonResponse({'success': False, 'error': 'Invalid request method'})
 
-def get_containers(api):
-    containers = []
-    for node in api.nodes.get():
-        node_containers = api.nodes(node['node']).lxc.get()
-        containers.extend(node_containers)
-    return containers
+@csrf_exempt
+def create_vms(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        vm_count = data.get("vm_count")
+
+        # Get other VM settings from data as necessary
+
+        proxmox = ProxmoxAPI(api_url, user=api_user, password=api_password, verify_ssl=False)
+
+        # Use the Proxmox API to create the VMs
+        for _ in range(vm_count):
+            # You can customize the VM creation process using the provided settings
+            # For example, you can create a new VM by cloning an existing one
+            # proxmox.nodes("pve1").qemu("100").clone.post(newid=next_vmid, name="new_vm_name")
+
+            # Or create a new VM from scratch using the settings from the form
+            # proxmox.nodes("pve1").qemu.post(vmid=next_vmid, name="new_vm_name", ...)
+            
+            # Make sure to increment the VM ID for each VM you create
+
+            pass
+
+        return JsonResponse({"status": "success"})
+    else:
+        return JsonResponse({"status": "error", "message": "Invalid request method"})
 
 def table(request):
     return render(request, 'vm_viewer/table.html')
